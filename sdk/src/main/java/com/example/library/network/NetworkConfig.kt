e2f035e28239adee3f79b4c0e3b81d7d601e80a1
Ankur <justgotoankur@gmail.com>
2024-03-11 11:11:53 +0530

    Commit: Project added to Repo

---
+++ sdk/src/main/java/com/example/library/network/NetworkConfig.kt
@@ -0,0 +1,11 @@
+package com.example.library.network
+
+/**
+ *  Created by ankur.khandelwal on 11/03/24
+ *
+ */
+internal class NetworkConfig {
+    companion object {
+        const val BASE_URL_DEV  = "https://credencedemo.acl-credence.com/"
+    }
+}
---
+++ sdk/src/main/java/com/example/library/network/UrlConstant.kt
@@ -0,0 +1,8 @@
+package com.example.library.network
+
+/**
+ *  Created by ankur.khandelwal on 11/03/24
+ *
+ */
+
+internal const val SAVE_FCM_TOKEN = "push/register"
